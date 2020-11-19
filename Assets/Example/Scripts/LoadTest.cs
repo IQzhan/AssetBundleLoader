866591c43fa23331a6a6339a2cfedd81ecfa2a48
ZhanQI <chinese778@foxmail.com>
2020-11-20 01:00:44 +0800

    Add Example

---
+++ Assets/Example/Scripts/LoadTest.cs
@@ -0,0 +1,49 @@
+﻿using E;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadTest : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void Create()
+    {
+        DontDestroyOnLoad(new GameObject("LoadTest").AddComponent<LoadTest>().gameObject);
+
+        //Example
+        //Load a scene
+        AssetBundleLoader.LoadScene("Example/Res/Scenes/Scene01/Scene01", (Scene scene01) =>
+        {
+            if (scene01 != default)
+            {
+                //Load an asset whith extension
+                AssetBundleLoader.LoadAsset("Example/Res/Prefabs/Sphere.prefab", (GameObject sphere) =>
+                {
+                    for(int i = 0; i < 20; i++)
+                    {
+                        GameObject obj = Instantiate(sphere);
+                        RandomMove randomMove = obj.GetComponent<RandomMove>();
+                        randomMove.during = Random.Range(0.03f, 0.18f);
+                    }
+                });
+            }
+        }, LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
---
+++ Assets/Example/Scripts/RandomMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    public Vector3 min;
+
+    public Vector3 max;
+
+    public float during = 0.1f;
+
+    private float currTime;
+
+    private Vector3 targetPos;
+
+    private void Update()
+    {
+        if(Time.time - currTime > during)
+        {
+            currTime = Time.time;
+            targetPos = new Vector3(
+                    Random.Range(min.x, max.x),
+                    Random.Range(min.y, max.y),
+                    Random.Range(min.z, max.z)
+                    );
+        }
+        transform.position = Vector3.Lerp(transform.position, targetPos, during);
+    }
+}
